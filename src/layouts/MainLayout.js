2d13afb640b85bdba289704c0d72c3af37b67394
Piyakarn Nimmakulvirut <jirakarnjim1@gmail.com>
2021-10-07 06:27:25 +0700

    Add navbar

---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,73 @@
+import Logo from 'assets/logo.png';
+
+const MainLayout = () => {
+  return (
+    <>
+      <div className='flex items-center h-16 px-10 bg-main align'>
+        <div className='px-3'>
+          <img className='w-28' src={Logo} alt='logo'></img>
+        </div>
+        <ul className='flex items-center lg:space-x-5'>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              Markets
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              Exchange
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              Buy Crypto
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              Contract
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              ETF
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              NewCoin
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              Kinghash
+            </a>
+          </li>
+          <li>
+            <a className='text-info hover:text-success' href='/'>
+              Announcement
+            </a>
+          </li>
+        </ul>
+        <div className='flex h-full ml-auto'>
+          <div className='flex px-6 py-4 border-l border-r lg:space-x-4 border-info'>
+            <button
+              type='button'
+              className='px-5 py-1 text-info hover:bg-info hover:bg-opacity-25'
+            >
+              Log In
+            </button>
+            <button
+              type='button'
+              className='px-5 py-1 border hover:text-orange text-info border-orange'
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      main: '#1d2635',
+      secondary: '#192531',
+      info: '#828ea1',
+      success: '#05ab63',
+      danger: '#eb4d5c',
+      orange: '#f67e2f',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
