070a22872f0114d6df3c3071f3579e3984a11494
KkyJim12 <jirakarnjim1@gmail.com>
2021-10-14 05:10:09 +0700

    Add attribute to marketlist

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -1,12 +1,17 @@
 import RightOutlined from "@ant-design/icons/RightOutlined";
 import SearchOutlined from "@ant-design/icons/SearchOutlined";
+import MenuUnfoldOutlined from "@ant-design/icons/MenuUnfoldOutlined";
 import StarOulined from "@ant-design/icons/StarOutlined";
 
-const MarketList = () => {
-  return (
-    <>
-      <div className="bg-main h-full">
-        <div className="px-4 pt-2 border-b border-info border-opacity-25 bg-third">
+const MarketList = (props) => {
+  const data = props.data;
+  const sideMenu = props.sideMenu;
+  const changeSideMenu = props.changeSideMenu;
+
+  const largeSideMenu = (
+    <div className="bg-main">
+      <div className="px-4 pt-2 border-b border-info border-opacity-25 bg-third">
+        <div className="flex">
           <div>
             <div className="absolute ml-2 text-info">
               <SearchOutlined />
@@ -17,58 +22,113 @@ const MarketList = () => {
               placeholder="Search"
             ></input>
           </div>
-          <div className="pt-2 flex">
-            <ul className="flex lg:space-x-5 text-info text-sm">
-              <li className=" border-b-2 border-main hover:border-orange pb-2">
-                <a href="/">Favorite</a>
-              </li>
-              <li className=" border-b-2 border-main hover:border-orange pb-2">
-                <a href="/">ETF</a>
-              </li>
-              <li className=" border-b-2 border-main hover:border-orange pb-2">
-                <a href="/">USDT</a>
-              </li>
-              <li className=" border-b-2 border-main hover:border-orange pb-2">
-                <a href="/">BTC</a>
-              </li>
-              <li className=" border-b-2 border-main hover:border-orange pb-2">
-                <a href="/">ETH</a>
-              </li>
-            </ul>
-            <span className="text-minor flex items-center ml-auto pb-2">
-              <RightOutlined />
-            </span>
+          <div className="ml-auto">
+            <button onClick={changeSideMenu} className="text-info">
+              <MenuUnfoldOutlined />
+            </button>
           </div>
         </div>
-        <div>
-          <table className="table-fixed w-full">
-            <thead className="text-info text-xs border-b border-info border-opacity-25">
-              <tr>
-                <th className="w-1/12"></th>
-                <th className="font-medium w-4/12 text-left py-1">Markets</th>
-                <th className="font-medium w-4/12 py-1 text-right">Price</th>
-                <th className="font-medium w-3/12 py-1">Change</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="">
-                <td className="">
-                  <div className="flex items-center justify-end text-info text-sm mr-1">
-                    <StarOulined />
-                  </div>
-                </td>
-                <td className="text-left text-white text-sm py-1">ETH</td>
-                <td className="text-right text-success text-xs py-1">
-                  1.00000000
-                </td>
-                <td className="text-center text-success text-xs py-1">+3.5%</td>
-              </tr>
-            </tbody>
-          </table>
+
+        <div className="pt-2 flex">
+          <ul className="flex lg:space-x-5 text-info text-sm">
+            <li className=" border-b-2 border-main hover:border-orange pb-2">
+              <a href="/">Favorite</a>
+            </li>
+            <li className=" border-b-2 border-main hover:border-orange pb-2">
+              <a href="/">ETF</a>
+            </li>
+            <li className=" border-b-2 border-main hover:border-orange pb-2">
+              <a href="/">USDT</a>
+            </li>
+            <li className=" border-b-2 border-main hover:border-orange pb-2">
+              <a href="/">BTC</a>
+            </li>
+            <li className=" border-b-2 border-main hover:border-orange pb-2">
+              <a href="/">ETH</a>
+            </li>
+          </ul>
+          <span className="text-minor flex items-center ml-auto pb-2">
+            <RightOutlined />
+          </span>
         </div>
       </div>
-    </>
+      <div className="overflow-y-scroll" style={{ height: "80vh" }}>
+        <table className="table-fixed w-full">
+          <thead className="text-info text-xs border-b border-info border-opacity-25">
+            <tr>
+              <th className="w-2/12"></th>
+              <th className="font-medium w-3/12 text-left py-1">Markets</th>
+              <th className="font-medium w-4/12 py-1 text-right">Price</th>
+              <th className="font-medium w-3/12 py-1 text-right">Change</th>
+              <th className="font-medium w-1/12 py-1"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => {
+              return (
+                <tr key={item.id} className="">
+                  <td className="">
+                    <div className="flex items-center justify-end text-info text-sm mr-1">
+                      <StarOulined />
+                    </div>
+                  </td>
+                  <td className="text-left text-white text-sm py-1">
+                    {item.symbol}
+                  </td>
+                  <td
+                    className={`text-right text-xs py-1 ${
+                      item.quote.USD.percent_change_24h > 0
+                        ? "text-success"
+                        : "text-danger"
+                    }`}
+                  >
+                    {item.quote.USD.price.toFixed(4)}
+                  </td>
+                  <td
+                    className={`text-right text-xs py-1 ${
+                      item.quote.USD.percent_change_24h > 0
+                        ? "text-success"
+                        : "text-danger"
+                    }`}
+                  >
+                    {item.quote.USD.percent_change_24h > 0 && "+"}
+                    {item.quote.USD.percent_change_24h.toFixed(2)}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
+
+  const smallSideMenu = (
+    <div className="flex flex-col">
+      <div className="flex justify-end px-2 py-1 bg-third">
+        <button className="text-info" onClick={changeSideMenu}>
+          <MenuUnfoldOutlined />
+        </button>
+      </div>
+      <div className="text-center bg-third text-white text-base py-1">USD</div>
+      <div className="text-center bg-main text-info text-sm border-t border-b border-info border-opacity-25 py-1">
+        Market
+      </div>
+      <div className="flex flex-col overflow-y-scroll" style={{ height: "80vh" }}>
+        {data.map((item, index) => {
+          return (
+            <a href="/">
+              <div className="text-center text-white py-1  text-xs hover:bg-third">
+                {item.symbol}
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+
+  return <>{sideMenu ? largeSideMenu : smallSideMenu}</>;
 };
 
 export default MarketList;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,20 +1,60 @@
+import { useEffect, useState } from "react";
 import MarketList from "components/MarketList";
 import Navbar from "components/Navbar";
 import Tradingview from "components/Tradingview";
 import Exchange from "components/Exchange";
 import Orderbook from "components/Orderbook";
 import MarketTrade from "components/MarketTrade";
+import axios from "axios";
 
 const MainLayout = () => {
+  const [marketList, setMarketList] = useState([]);
+  const [sideMenu, setSideMenu] = useState(true);
+
+  useEffect(() => {
+    getMarketList();
+  }, []);
+
+  const changeSideMenu = () => {
+    setSideMenu(!sideMenu);
+  };
+
+  const getMarketList = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + "/v1/cryptocurrency/listings/latest",
+        {
+          headers: {
+            "X-CMC_PRO_API_KEY": process.env.REACT_APP_API_KEY,
+          },
+        }
+      );
+      console.log(response.data.data);
+      setMarketList(response.data.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
     <div className="bg-secondary">
       <Navbar />
       <div className="h-screen">
-        <div className="grid grid-cols-6 h-5/6">
-          <div className="col-span-1 p-1">
-            <MarketList />
+        <div className="grid grid-cols-12" style={{ height: "80vh" }}>
+          <div
+            className={`${sideMenu ? "col-span-2 " : "col-span-1"} p-1 bg-main`}
+          >
+            <MarketList
+              data={marketList}
+              sideMenu={sideMenu}
+              changeSideMenu={changeSideMenu}
+            />
           </div>
-          <div className="col-span-3 p-1 flex flex-col lg:space-y-1">
+          <div
+            className={`${
+              sideMenu ? "col-span-6" : "col-span-7"
+            } p-1 flex flex-col lg:space-y-1`}
+          >
             <div className="flex-1">
               <Tradingview />
             </div>
@@ -22,10 +62,10 @@ const MainLayout = () => {
               <Exchange />
             </div>
           </div>
-          <div className="col-span-1 p-1">
+          <div className="col-span-2 p-1">
             <Orderbook />
           </div>
-          <div className="col-span-1 p-1">
+          <div className="col-span-2 p-1">
             <MarketTrade />
           </div>
         </div>
